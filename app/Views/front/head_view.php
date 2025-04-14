ec63814dbbc085692074f2b341905e593495da83
Yolanda <yolanda2002.ya@gmail.com>
2025-04-14 15:56:38 -0300

    Estado inicial del proyecto

---
+++ app/Views/front/head_view.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset = "utf-8">
+    <meta name = "viewport" content = width=device-width, initial-scale=1">
+    <title><?php echo($titulo);?></title>
+
+    <!--bootstrap-->
+    <link href = "assets/css/bootstrap.min.css" rel ="stylesheet" >
+    <script src = "assets/js/bootstrap.bundle.min.js" ></script>
+
+<!--mi hoja de estilo-->
+ <link href="assests/css/miestilo.css" type= "text/css" rel="stylesheet">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/...../6.4.0/css/all.min.css">
+</head>
---
+++ app/Views/front/nueva_plantilla.php
@@ -0,0 +1,61 @@
+
+<body>
+    <section class ="information">
+    <h1> valen & vir </h1>
+    <h2> Pasteleria Artesanal</h2>  
+    </section>
+
+    <section class ="carrusel">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets\img\TORTA.jpg" class="d-block w-100"  alt="tortas">
+    </div>
+    <div class="carousel-item">
+      <img src="assets\img\TARTA_MANZ.jpg" class="d-block w-100"  alt="mesa de dulces">
+    </div>
+    <div class="carousel-item">
+      <img src="assets\img\TORTA.jpg" class="d-block w-100"  alt="tartas">
+    </div>
+  </div>
+  </div>
+</section>
+
+  <section class ="categoria">
+  <h2> Categorias</h2>
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="Torta de bodas">
+      <div class="card-body">
+        <h5 class="Tortas de boda">Card title</h5>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="Tortas infantiles">torta</h5>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="Tartas Frutales">Card title</h5>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="Mesa de dulces">Card title</h5>
+      </div>
+    </div>
+  </div>
+</div>
+
+</html>
