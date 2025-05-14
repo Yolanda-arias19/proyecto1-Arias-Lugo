ca361aa41a9e3e15eb521d389f966cbaee41b436
Renata <lugorenata26@gmail.com>
2025-05-14 11:50:20 -0300

    arreglo de algunos ajustes

--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -50,6 +50,7 @@
 <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 <!--Javascript de bootstrap-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
