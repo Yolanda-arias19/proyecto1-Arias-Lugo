ea86ab0dd0e0c362b968b860268f0371bef549db
Renata <lugorenata26@gmail.com>
2025-05-31 18:56:19 -0300

    navegador

--- app/Views/front/nav_view.php
+++ app/Views/front/nav_view.php
@@ -3,7 +3,7 @@
   <?php
   $session = session();
   $nombre = $session->get('nombre');
-  $perfil = $session->gwt('perfil_id'); ?>
+  $perfil = $session->get('perfil_id'); ?>
 
 <!--Navegador-->
 <nav class="navbar navbar-expand-lg fixed-top">
@@ -19,11 +19,10 @@
         <h5 class="offcanvas-title" id="offcanvasNavbarLabel">V&V</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-
+      <div class="offcanvas-body">
 <!--Barra de navegación admin-->
     <?php if($perfil == 1): ?>
       <div class="btn btn-info active btnUser btn-sm"><a href="">USUARIO: <?php echo session('nombre'); ?></a></div>
-        <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
           <li class="nav-item">
             <a class="nav-link mx-lg-2" href="<?php echo base_url('quienes_somos');?>">Quienes somos</a>
@@ -64,32 +63,15 @@
               <li><a class="dropdown-item" href="#">CRUD productos</a></li>
               <li><a class="dropdown-item" href="#">Muestra ventas</a></li>
               <li><a class="dropdown-item" href="#">Consultas</a></li>
-              <li><a class="dropdown-item" href="#">Cerrar sesion</a></li>
             </ul>
           </li>
           <!--Fin del menu desplegable operacines admin-->
         </ul>
-      </div>
-
-
-
-
-
-
-
-
-
-
-
 
 
       <!--Aca es la del cliente-->
       <?php elseif($perfil == 2): ?>
         <div class="btn btn-info active btnUser btn-sm"><a href="">CLIENTE: <?php echo session('nombre'); ?></a></div>
-
-      <!--Si no es ninguno de los dos-->
-<!--original-->
-      <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
           <li class="nav-item">
             <a class="nav-link mx-lg-2" href="<?php echo base_url('quienes_somos');?>">Quienes somos</a>
@@ -120,12 +102,51 @@
           </li>
           <!--Fin del menu desplegable-->
         </ul>
+
+      <!--Si no es ninguno de los dos-->
+      <?php else: ?>
+
+        <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link mx-lg-2" href="<?php echo base_url('quienes_somos');?>">Quienes somos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-lg-2" href="<?php echo base_url('contacto');?>">Contactos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-lg-2" href="<?php echo base_url('comercializacion');?>">Comercialización</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-lg-2" href="<?php echo base_url('consultas');?>">Consultas</a>
+          </li>
+<!--Menu desplegable-->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Categorías
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Tortas</a></li>
+              <li><a class="dropdown-item" href="#">Postres</a></li>
+              <li><a class="dropdown-item" href="#">Tartas</a></li>
+              <li><a class="dropdown-item" href="#">Alfajores</a></li>
+              <li><a class="dropdown-item" href="#">Cupcakes</a></li>
+              <li><a class="dropdown-item" href="#">Galletas</a></li>
+              <li><a class="dropdown-item" href="#">Donas</a></li>
+            </ul>
+          </li>
+          <!--Fin del menu desplegable-->
+        </ul>
+                <?php endif; ?>
       </div>
     </div>
-      <a class="btnLogin-popup" href="<?php echo base_url('login');?>">
-      Iniciar sesión
-      </a>
-  
+    
+          <div class="d-flex">
+                <?php if (empty($nombre)): ?>
+                      <a class="btnLogin-popup" href="<?php echo base_url('login');?>">Iniciar sesión</a>
+                    <?php else: ?>
+                      <a class="btn btn-danger" href="<?php echo base_url('logout');?>">Cerrar sesión</a>
+                <?php endif; ?>
+                </div>
   </div>
 </nav>
 
